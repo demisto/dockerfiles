e19ccb6d5600b93ede2eb0df004479152eeb84bb
Agam More <agam.more@demisto.com>
2019-09-18 10:57:37 +0300

    Update sane doc reports

--- docker/sane-doc-reports/src/tests/library/elements/test_table.py
+++ docker/sane-doc-reports/src/tests/library/elements/test_table.py
@@ -19,3 +19,19 @@ def test_table_in_report():
 
     # Check that it has the right amount of rows
     assert len(d.element.xpath('//w:tbl//w:tbl//w:t')) == 22
+
+
+def test_table_in_report():
+    report = Report(*_transform('elements/table_widget.json'))
+    report.populate_report()
+    d = report.document
+    table = next(utils.iter_block_items(d))
+    assert isinstance(table, Table)
+    assert len(table.columns) == 12
+    assert len(table.rows) == 3
+
+    # Check that there is indeed an image
+    assert len(d.element.xpath('//w:tbl//w:tbl')) == 1
+
+    # Check that it has the right amount of rows
+    assert len(d.element.xpath('//w:t[contains(text(), "Eve listens")]')) == 1
