bb125d914935a50b5f7117ea66f98ce2b0c1a7a6
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-01-13 12:52:53 +0200

    verify ssl and update comments

--- .github/workflows/notify-docker-available/notify.py
+++ .github/workflows/notify-docker-available/notify.py
@@ -12,24 +12,44 @@ import os
 LINE_BEFORE_MESSAGE = 'Creating artifact of docker image...'
 LINE_AFTER_MESSAGE = 'Skipping docker push for cr'
 
+VERIFY_SSL = not (os.getenv('VERIFY_SSL') and os.getenv('VERIFY_SSL').lower() in ('false', '0', 'no'))
+
+if not VERIFY_SSL:
+    requests.packages.urllib3.disable_warnings()
+
 
 def post_comment(docker_build_msg, pr_num):
-    post_url = "https://api.github.com/repos/demisto/dockerfiles/issues/{}/comments".format(pr_num)
+    commnets_url = "https://api.github.com/repos/demisto/dockerfiles/issues/{}/comments".format(pr_num)
     token = os.getenv('GITHUB_TOKEN')
     if not token:
         raise ValueError("Can't post comment. GITHUB_TOKEN env variable is not set")
+    title = "# Docker Image Ready - Dev"
+    user_prefix = "github-actions"
     message = (
-        "# Docker Image Ready - Dev\n\n" +
+        title + "\n\n" +
         "Docker automatic build at CircleCI has completed. The docker image is available as an artifact of the build.\n\n" +
         "Follow the output from the build on how to load the image locally for testing:\n\n" +        
         docker_build_msg +
         "\n\n"
     )
-    res = requests.post(post_url, json={"body": message}, headers={'Authorization': 'Bearer ' + token})
-    try:
-        res.raise_for_status()
-    except Exception as ex:
-        print("Failed comment post: {}".format(ex))
+    headers = {'Authorization': 'Bearer ' + token}
+    # search for existing if so we will update
+    res = requests.get(commnets_url, headers=headers, verify=VERIFY_SSL)
+    replace_comment_id = None
+    if res.status_code == 200:
+        comments = res.json()
+        for c in comments:
+            if (c.get('user') and c.get('user').get('login') and c['user']['login'].startswith(user_prefix)
+               and c.get('body') and c['body'].startswith(title)):
+                replace_comment_id = c.get('id')
+                print(f'replacing comment: {c}')
+                break
+    if replace_comment_id:
+        commnets_url = f'https://api.github.com/repos/demisto/dockerfiles/issues/comments/{replace_comment_id}'
+        res = requests.patch(commnets_url, json={"body": message}, headers=headers, verify=VERIFY_SSL)
+    else:
+        res = requests.post(commnets_url, json={"body": message}, headers=headers, veify=VERIFY_SSL)
+    res.raise_for_status()
 
 
 def check_docker_build(event_file):
@@ -41,7 +61,7 @@ def check_docker_build(event_file):
     target_url = target_url.split('?')[0]
     build_num = target_url.split('/')[-1]
     print(f'circleci build: {build_num}')
-    res = requests.get(f'https://circleci.com/api/v1.1/project/github/demisto/dockerfiles/{build_num}')
+    res = requests.get(f'https://circleci.com/api/v1.1/project/github/demisto/dockerfiles/{build_num}', verify=VERIFY_SSL)
     res.raise_for_status()
     build_json = res.json()
     # check that this is a pull request
