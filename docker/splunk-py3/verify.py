e560fef993a426e938ada99c58c4460790896d91
esharf <esahrf@paloaltonetworks.com>
2020-04-02 08:29:19 +0300

    rename to splunk-py3 and adding verify script

---
+++ docker/splunk-py3/verify.py
@@ -0,0 +1,9 @@
+import splunklib
+
+try:
+    splunklib.client.connect()
+except ConnectionRefusedError as error:
+    if not error.strerror == 'Connection refused':
+        raise error
+else:
+    raise Exception('failed')
