b32085d2ca7dda7931bb1b2da0c0ec0056a21a5f
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-04-14 12:46:17 +0300

    move to debian base and install oracle driver

--- docker/genericsql/verify.py
+++ docker/genericsql/verify.py
@@ -6,4 +6,12 @@ import cx_Oracle
 
 assert 'FreeTDS' in pyodbc.drivers()
 
+try:
+    # make sure oracle manages to load tns client libraries. 
+    # Will fail but we want to be sure we don't fail on loading the driver
+    cx_Oracle.connect()  
+except Exception as ex:
+    assert 'TNS:net service name is incorrectly specified' in str(ex)
+
+
 print("All is good. All imported successfully")
