ee9a4a709742b00841cbb522151caaa5eb580c8e
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-04-16 12:38:32 +0300

    Pwsh create script support + pwsh-azure image (#1643)
    * verify license pwsh support
    
    * powershell support
    
    * pwsh-azure image
    
    * set pyenv versions
    
    * use py3.7 for verify licenses
    
    * py3 again
    
    * powershell readme
    
    * add requests to py 3.7
    
    * Fix quote
    
    * Typo
    
    * rever dockerbuild. will handle in seperate pr
    
    * update to new build image
    
    * update dockerbuild to last
    
    * Update README.md
    
    Co-Authored-By: Itay Keren <ikeren@paloaltonetworks.com>
    
    * Update README.md
    
    Co-Authored-By: Itay Keren <ikeren@paloaltonetworks.com>
    
    * Update README.md
    
    Co-Authored-By: Itay Keren <ikeren@paloaltonetworks.com>
    
    * add link to repo info of base images
    
    * Update README.md
    
    Co-Authored-By: Itay Keren <ikeren@paloaltonetworks.com>
    
    * Update docker/verify_licenses.py
    
    Co-Authored-By: Itay Keren <ikeren@paloaltonetworks.com>
    
    * Update docker/verify_licenses.py
    
    Co-Authored-By: Itay Keren <ikeren@paloaltonetworks.com>
    
    Co-authored-by: Itay Keren <ikeren@paloaltonetworks.com>

--- docker/create_new_docker_image.py
+++ docker/create_new_docker_image.py
@@ -9,7 +9,7 @@ import string
 import shutil
 import subprocess
 
-DOCKER_ALPINE = '''
+DOCKER_PYTHON_ALPINE = '''
 FROM {image}
 
 COPY requirements.txt .
@@ -19,7 +19,7 @@ RUN apk --update add --no-cache --virtual .build-dependencies python-dev build-b
   && apk del .build-dependencies
 '''
 
-DOCKER_DEBIAN = '''
+DOCKER_PYTHON_DEBIAN = '''
 FROM {image}
 
 COPY requirements.txt .
@@ -32,6 +32,13 @@ RUN apt-get update && apt-get install -y --no-install-recommends \\
   gcc \\
   python-dev \\
 && rm -rf /var/lib/apt/lists/*
+'''
+
+DOCKER_POWERSHELL = '''
+FROM {image}
+
+RUN pwsh -c "Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -ErrorAction Stop"
+RUN pwsh -c "Install-Module -ErrorAction Stop {modules}"
 '''
 
 
@@ -62,43 +69,17 @@ def get_latest_tag(image_name):
     return last_tag
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Create a new python based docker image',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-p", "--python", help="Specify python version to use",
-                        choices=['two', 'three'], default='three')
-    parser.add_argument("-l", "--linux", help="Specify linux distro to use",
-                        choices=['alpine', 'debian'], default='alpine')
-    parser.add_argument('--pkg', action='append', help='Specify a python package to install. Can be specified multiple times. ' +
-                        'Each package needs to be specified with --pkg. For example: --pkg google-cloud-storage --pkg oath2client')
-    parser.add_argument("name", help="The image name to use without the organization prefix. For example: ldap3")
-
-    args = parser.parse_args()
-
-    version = "" if args.python == 'two' else '3'
-    linux = "" if args.linux == 'alpine' else '-deb'
-
-    base_image = "demisto/python{}{}".format(version, linux)
-
-    print(args)
-    print(sys.path[0])
-    print("Using base image: {}".format(base_image))
-    folder = "{}/{}".format(sys.path[0], args.name)
-    if os.path.exists(folder):
-        sys.stderr.write('Error: Folder [{}] already exists. Must specify a new image name.\n'.format(folder))
-        sys.exit(1)
-
-    last_tag = get_latest_tag(base_image)
-    base_image_last = "{}:{}".format(base_image, last_tag)
-    print("Latest base image: " + base_image_last)
-    os.mkdir(folder)
-    docker_template = DOCKER_ALPINE if args.linux == 'alpine' else DOCKER_DEBIAN
+def create_powershell_image(folder, base_image, args):
     docker_file = open(folder + "/Dockerfile", "w+")
-    docker_file.write(docker_template.format(image=base_image_last))
+    docker_file.write(DOCKER_POWERSHELL.format(image=base_image, modules=",".join(args.pkg)))
     docker_file.close()
-    conf_file = open(folder + "/build.conf", "w+")
-    conf_file.write("version=1.0.0\n")
-    conf_file.close()
+
+
+def create_python_image(folder, base_image, args):
+    docker_template = DOCKER_PYTHON_ALPINE if args.linux == 'alpine' else DOCKER_PYTHON_DEBIAN
+    docker_file = open(folder + "/Dockerfile", "w+")
+    docker_file.write(docker_template.format(image=base_image))
+    docker_file.close()
     # copy gitignore from python image
     shutil.copy(sys.path[0] + "/python/.gitignore", folder)
     print("Initializing pipenv...")
@@ -114,11 +95,59 @@ def main():
         subprocess.call(cmd_arr, cwd=folder, env=my_env)
     else:
         subprocess.call(["pipenv", "lock"], cwd=folder, env=my_env)
+    print("NOTE: To install additional python packages: cd {}; pipenv install <package>".format(folder))
+
+
+LINUX_DIST_TO_SUFFIX = {
+    'alpine': '',
+    'debian': '-deb',
+    'ubuntu': '-ubuntu'
+}
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Create a new docker image',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-t", "--type", help="Specify type of image to create",
+                        choices=['python', 'powershell'], default='python')
+    parser.add_argument("-p", "--python", help="Specify python version to use",
+                        choices=['two', 'three'], default='three')
+    parser.add_argument("-l", "--linux", help="Specify linux distro to use",
+                        choices=['alpine', 'debian', 'ubuntu'], default='alpine')
+    parser.add_argument('--pkg', action='append', help='Specify a package to install. Can be specified multiple times. ' +
+                        'Each package needs to be specified with --pkg. For example: --pkg google-cloud-storage --pkg oath2client')
+    parser.add_argument("name", help="The image name to use without the organization prefix. For example: ldap3")
+
+    args = parser.parse_args()
+
+    version = "" if (args.type == 'powershell' or args.python == 'two') else '3'
+    linux = LINUX_DIST_TO_SUFFIX[args.linux]
+
+    base_image = "demisto/{}{}{}".format(args.type, version, linux)
+
+    print(args)
+    print("docker dir: ".format(sys.path[0]))
+    print("Using base image: {}".format(base_image))
+    folder = "{}/{}".format(sys.path[0], args.name)
+    if os.path.exists(folder):
+        sys.stderr.write('Error: Folder [{}] already exists. Must specify a new image name.\n'.format(folder))
+        sys.exit(1)
+
+    last_tag = get_latest_tag(base_image)
+    base_image_last = "{}:{}".format(base_image, last_tag)
+    print("Latest base image: " + base_image_last)
+    os.mkdir(folder)
+    conf_file = open(folder + "/build.conf", "w+")
+    conf_file.write("version=1.0.0\n")
+    conf_file.close()
+    if args.type == "python":
+        create_python_image(folder, base_image_last, args)
+    else:
+        create_powershell_image(folder, base_image_last, args)
     print('Adding: {} to .dependabot/config.yml ...'.format(folder))
     subprocess.check_call([sys.path[0] + "/add_dependabot.sh", "docker/" + args.name])
     print('========================================')
     print("Done creating image files in folder: " + folder)
-    print("\nTo install additional python packages: cd {}; pipenv install <package>".format(folder))
     print("\nTo build locally the docker image run: {}/build_docker.sh {}".format(sys.path[0], args.name))
 
 
