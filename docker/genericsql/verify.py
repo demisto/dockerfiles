ee00078e882e4897e7b6811f0d028a459abf789a
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-04-19 21:50:33 +0300

    fix odbcinst freetds location (#1652)

--- docker/genericsql/verify.py
+++ docker/genericsql/verify.py
@@ -14,4 +14,11 @@ except Exception as ex:
     assert 'TNS:net service name is incorrectly specified' in str(ex)
 
 
+# freetds test
+engine = sqlalchemy.create_engine('mssql+pyodbc:///testuser:testpass@127.0.0.1:1433/TEST?driver=FreeTDS')
+try:
+    engine.execute('select 1 as [Result]')
+except Exception as ex:
+    assert "Can't open lib" not in str(ex), "Failed because of missing lib: " + str(ex)    
+
 print("All is good. All imported successfully")
