0d574cf59eacdd3dc727fb7c475688e925c2688f
yorhov <Orekhova97229!>
2020-05-26 10:35:51 +0300

    added tldextract to verfiy.py

--- docker/python3/verify.py
+++ docker/python3/verify.py
@@ -1,7 +1,9 @@
 import warnings
 import dateparser
-
+import tldextract
 # set warnings to throw an error
 warnings.simplefilter("error")
 d = dateparser.parse('1 day')
+extract = tldextract.TLDExtract(cache_file=False,suffix_list_urls=None)
+t = extract('*test-test.com')
 print("all is good managed to parse: {}".format(d))
