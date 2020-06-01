4991aeeae75226d13e5a2ca942c4b5041da11fc4
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-06-01 08:46:22 +0300

    add filters to images being updated

--- docker/update_images_from_python_base.sh
+++ docker/update_images_from_python_base.sh
@@ -7,15 +7,19 @@ set -e
 # Go over all images and update python base as necessary.
 #######################################
 
+## Images which should be updated in a seperate PR (grep regex)
+SEPERATE_PR="docker/chromium/|docker/pcap-http-extractor/|docker/office-utils/|docker/snowflake/|docker/zeeep/"
+
+# dev only images to ignore
+temp_dev=$(mktemp)
+grep -l  "devonly=true" docker/*/build.conf | xargs -n 1 dirname > $temp_dev
+
+
 for image in python python3 python-deb python3-deb ; do
     full_image="demisto/$image"    
     echo "Processing image: [$full_image]"
     latest_tag=$(./docker/image_latest_tag.py $full_image)
     echo "latest tag: $latest_tag. Updating images..."
-    grep -l  -E "FROM\\s+${full_image}:" docker/*/Dockerfile  | xargs sed -i '' -e "s#${full_image}:.*#${full_image}:${latest_tag}#"
+    grep -l  -E "FROM\\s+${full_image}:" docker/*/Dockerfile | grep -v "$SEPERATE_PR" | grep -v -f $temp_dev
+    # xargs sed -i '' -e "s#${full_image}:.*#${full_image}:${latest_tag}#"
 done
-
-
-# update to latest tag
-# grep -l  -E 'FROM\s+demisto/crypto' docker/*/Dockerfile  | xargs sed -i '' -e "s#demisto/crypto:.*#demisto/crypto:${CYPRTO_TAG}#"
-
