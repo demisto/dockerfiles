7d9c4183ce2e8e542e616bb4c782a8d7a6a8d67c
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-06-01 11:49:39 +0300

    ignore faker3

--- docker/update_images_from_python_base.sh
+++ docker/update_images_from_python_base.sh
@@ -8,7 +8,7 @@ set -e
 #######################################
 
 ## Images which should be updated in a seperate PR (grep regex)
-SEPERATE_PR="docker/chromium/|docker/pcap-http-extractor/|docker/office-utils/|docker/snowflake/|docker/zeep/"
+SEPERATE_PR="/chromium/|/pcap-http-extractor/|/office-utils/|/snowflake/|/zeep/|/faker3/"
 
 # dev only images to ignore
 temp_dev=$(mktemp)
