aafe506f13684bb6bc8468c6b5a79f484aa2d576
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-09-29 19:21:18 +0300

    update python2 base docker images

--- docker/update_images_from_python_base.sh
+++ docker/update_images_from_python_base.sh
@@ -10,12 +10,32 @@ set -e
 ## Images which should be updated in a seperate PR (grep regex)
 SEPERATE_PR="/pcap-http-extractor/|/office-utils/|/snowflake/|/faker3/"
 
+if [[ -z "$1" ]] || [[ "$1" == -* ]]; then    
+    echo "Usage: ${BASH_SOURCE} <python_version (2 or 3)>"
+    echo ""
+    echo "Update docker images when there are new base docker images available. Create seperate PRs for python 2 and 3 as the build otherwise can time out."
+    echo ""
+    echo "For example: ${BASH_SOURCE} 2"
+    exit 1
+fi
+
+
+if [[ "$1" == "2" ]]; then
+    BASE_IMAGES="python python-deb"
+elif [[ "$1" == "3" ]]; then
+    BASE_IMAGES="python3 python3-deb"
+else
+    echo "Error: Unknown python version specified: $1"
+    exit 2
+fi
+echo "Starting update of python $1 images using the following base images: $BASE_IMAGES"
+
 # dev only images to ignore
 temp_dev=$(mktemp)
 grep -l  "devonly=true" docker/*/build.conf | xargs -n 1 dirname > $temp_dev
 
 
-for image in python python3 python-deb python3-deb ; do
+for image in `echo $BASE_IMAGES` ; do
     full_image="demisto/$image"    
     echo "Processing image: [$full_image]"
     latest_tag=$(./docker/image_latest_tag.py $full_image)
