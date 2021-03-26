e856ff351151c46aada7c874b22456c40e24fd55
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2021-03-26 18:47:46 +0300

    add support for patch cve-2021-3177

--- docker/powershell-core/verify.py
+++ docker/powershell-core/verify.py
@@ -9,3 +9,9 @@ print("all is good managed to parse: {}".format(d))
 
 res = subprocess.check_output(['pwsh', '--version'])
 assert res.startswith('PowerShell 7')
+
+# cve-2021-3177 See: https://bugs.python.org/issue42938
+# verify we are patched
+from ctypes import *
+c_double.from_param(1e300)
+print("all is good")
