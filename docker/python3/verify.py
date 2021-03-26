212b74a662ea89076de1d05a47e379ffa80e5169
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2021-03-26 18:59:06 +0300

    python base update

--- docker/python3/verify.py
+++ docker/python3/verify.py
@@ -7,3 +7,9 @@ d = dateparser.parse('1 day')
 extract = tldextract.TLDExtract(cache_file=False,suffix_list_urls=None)
 t = extract('*test-test.com')
 print("all is good managed to parse: {}".format(d))
+
+# cve-2021-3177 See: https://bugs.python.org/issue42938
+# verify we are patched
+from ctypes import *
+c_double.from_param(1e300)
+print("all is good cve-2021-3177 is patched")
