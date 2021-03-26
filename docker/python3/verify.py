6bb20b284309452228b77883f843695ef689370d
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2021-03-26 19:17:01 +0300

    fix patch verify

--- docker/python3/verify.py
+++ docker/python3/verify.py
@@ -11,5 +11,5 @@ print("all is good managed to parse: {}".format(d))
 # cve-2021-3177 See: https://bugs.python.org/issue42938
 # verify we are patched
 from ctypes import *
-c_double.from_param(1e300)
+print(c_double.from_param(1e300))
 print("all is good cve-2021-3177 is patched")
