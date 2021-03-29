b017c2f3965b5d62f6854844092669f3ee29e657
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2021-03-29 10:09:49 +0300

    limit PR size

--- docker/update_images_from_python_base.sh
+++ docker/update_images_from_python_base.sh
@@ -14,6 +14,7 @@ if [[ -z "$1" ]] || [[ "$1" == -* ]]; then
     echo "Usage: ${BASH_SOURCE} <python_version (2 or 3)>"
     echo ""
     echo "Update docker images when there are new base docker images available. Create seperate PRs for python 2 and 3 as the build otherwise can time out."
+    echo "Will further limit to 25 updates per base image to avoid huge PRs."
     echo ""
     echo "For example: ${BASH_SOURCE} 2"
     exit 1
@@ -34,13 +35,14 @@ echo "Starting update of python $1 images using the following base images: $BASE
 temp_dev=$(mktemp)
 grep -l  "devonly=true" docker/*/build.conf | xargs -n 1 dirname > $temp_dev
 
+MAX_IMAGES=25
 
 for image in `echo $BASE_IMAGES` ; do
     full_image="demisto/$image"    
     echo "Processing image: [$full_image]"
     latest_tag=$(./docker/image_latest_tag.py $full_image)
     echo "latest tag: $latest_tag. Updating images..."
-    grep -l  -E "FROM\\s+${full_image}:" docker/*/Dockerfile | grep -v -E "$SEPERATE_PR" | grep -v -f $temp_dev | xargs sed -i '' -e "s#${full_image}:.*#${full_image}:${latest_tag}#"
+    grep -l  -E "FROM\\s+${full_image}:" docker/*/Dockerfile | grep -v -E "$SEPERATE_PR" | grep -v -f $temp_dev | head -$MAX_IMAGES | xargs sed -i '' -e "s#${full_image}:.*#${full_image}:${latest_tag}#"
 done
 
 rm $temp_dev
