9aa9d96e9f36056dd94cb010c7fe7be3fe230ec5
syaakovi <syaakovi@paloaltonetworks.com>
2021-04-22 19:25:40 +0300

    added fixed version to pymisp and added validation file

---
+++ docker/pymisp/verify.py
@@ -0,0 +1,5 @@
+# starting pymisp v2.4.135.2, EmailObject had optional dependencies which failed the import if not present
+from pymisp.tools import EMailObject, GenericObjectGenerator
+from pymisp import ExpandedPyMISP, PyMISPError, MISPObject
+
+print('Everything is awesome!')
