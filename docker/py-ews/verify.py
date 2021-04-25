0c7cbd5d21d868d2e5f119bb5be75fe2cf266cf4
bachen <bachen@paloaltonetworks.com>
2021-04-25 10:23:58 +0300

    fix lxml

---
+++ docker/py-ews/verify.py
@@ -0,0 +1,15 @@
+import dateparser
+import exchangelib
+import email
+import hashlib
+import subprocess
+import warnings
+from collections import deque
+from multiprocessing import Process
+
+
+
+
+
+
+
