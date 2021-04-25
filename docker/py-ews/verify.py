9018c41d0c5d8c0e2ac07765460f325f415ec05e
bachen <bachen@paloaltonetworks.com>
2021-04-25 11:11:57 +0300

    update verify.py

--- docker/py-ews/verify.py
+++ docker/py-ews/verify.py
@@ -1,5 +1,3 @@
-import dateparser
-import exchangelib
 import email
 import hashlib
 import subprocess
@@ -7,9 +5,27 @@ import warnings
 from collections import deque
 from multiprocessing import Process
 
-
-
-
-
-
-
+import dateparser
+import exchangelib
+from cStringIO import StringIO
+from exchangelib import (BASIC, DELEGATE, DIGEST, IMPERSONATION, NTLM, Account,
+                         Body, Build, Configuration, Credentials, EWSDateTime,
+                         EWSTimeZone, FileAttachment, Folder, HTMLBody,
+                         ItemAttachment, Version)
+from exchangelib.errors import (AutoDiscoverFailed, ErrorFolderNotFound,
+                                ErrorInvalidIdMalformed,
+                                ErrorInvalidPropertyRequest,
+                                ErrorIrresolvableConflict, ErrorItemNotFound,
+                                ErrorMailboxMoveInProgress,
+                                ErrorMailboxStoreUnavailable,
+                                ErrorNameResolutionNoResults, RateLimitError,
+                                ResponseMessageError, TransportError)
+from exchangelib.items import Contact, Item, Message
+from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
+from exchangelib.services import EWSAccountService, EWSService
+from exchangelib.util import add_xml_child, create_element
+from exchangelib.version import (EXCHANGE_2007, EXCHANGE_2010,
+                                 EXCHANGE_2010_SP2, EXCHANGE_2013,
+                                 EXCHANGE_2016)
+from future import utils as future_utils
+from requests.exceptions import ConnectionError
