1ce7643f7bdbf6eb4d2086f34ba73a2122b60876
glicht <glicht@users.noreply.github.com>
2021-05-19 00:29:39 +0300

    increase read timeout and add nginx -t test to verify.py

--- docker/flask-nginx/verify.py
+++ docker/flask-nginx/verify.py
@@ -4,3 +4,7 @@ res = subprocess.check_output(['nginx', '-V'], text=True, stderr=subprocess.STDO
 print(res)
 # verify that we are using nginx 1.x
 assert 'nginx/1.' in res
+
+# verify that nginx test passes
+res = subprocess.check_output(['nginx', '-t'], text=True, stderr=subprocess.STDOUT)
+print(res)
