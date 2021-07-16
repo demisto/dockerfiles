6b72a229a495104bf97e33e95ac8ae524bca1591
glicht <glicht@users.noreply.github.com>
2021-07-16 14:36:42 +0300

    update verify and make sure it fails

--- docker/powershell/verify.ps1
+++ docker/powershell/verify.ps1
@@ -1,6 +1,9 @@
 Write-Host("Verify script is running")
-Write-Output("All good. pwsh is setup fine!")
+
+Get-TimeZone
 
 if ($(Get-TimeZone).Id -ne 'Etc/UTC') {
     throw "TimeZone.Id not equal to Etc/UTC. Value: " + $(Get-TimeZone).Id
 }
+
+Write-Output("All good. pwsh is setup fine!")
