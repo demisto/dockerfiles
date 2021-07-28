a77ba73f319694bc9379e18c5bd79db7fe98c666
Wissam Ghammashi <wghammashi@paloaltonetworks.com>
2021-07-28 20:16:10 +0300

    fix the dockerfile_parser init

--- ironbank/build_dockerfile.py
+++ ironbank/build_dockerfile.py
@@ -8,7 +8,7 @@ from ironbank.constants import DockerfileMetadata, DockerfileSections
 
 
 class DockerfileIronBank:
-    
+
     def __init__(self, docker_image_dir, output_path):
         self.docker_image_dir = docker_image_dir
         self.docker_image_name = os.path.basename(self.docker_image_dir)
@@ -17,26 +17,29 @@ class DockerfileIronBank:
 
     def build(self):
         src_dockerfile_content = get_dockerfile_content(os.path.join(self.docker_image_dir, DockerfileMetadata.FILENAME))
-        src_dockerfile_parser = DockerfileParser()
-        src_dockerfile_parser.content=src_dockerfile_content
+        src_dockerfile_parser = DockerfileParser(os.path.join(self.docker_image_dir, DockerfileMetadata.FILENAME))
+        #src_dockerfile_parser.content(os.path.join(self.docker_image_dir, DockerfileMetadata.FILENAME))
         dst_dockerfile = os.path.join(self.output_path, DockerfileMetadata.FILENAME)
-        baseImage, baseTag = src_dockerfile_parser.baseimage.split(":")
+        base_image, base_tag = src_dockerfile_parser.baseimage.split(":")
+        print(f"Converting {os.path.join(self.docker_image_dir, DockerfileMetadata.FILENAME)} into {dst_dockerfile}")
         with open(dst_dockerfile, "w") as f:
-            f.write(DockerfileSections.HEADER.format(self.base_images_repo.get_inventory()[baseImage][0], baseTag))
+            f.write(DockerfileSections.HEADER.format(self.base_images_repo.get_inventory()[base_image][0], base_tag))
             f.write(DockerfileSections.FILE_BLANK_LINE)
             f.write(DockerfileSections.COPY_REQS_TXT)
             f.write(DockerfileSections.FILE_BLANK_LINE)
-            f.write(DockerfileSections.DNF_UPDATE_BASIC_PY.format(self.base_images_repo.get_inventory()[baseImage][1]))
+            f.write(DockerfileSections.DNF_UPDATE_BASIC_PY.format(self.base_images_repo.get_inventory()[base_image][1]))
             f.write(DockerfileSections.FILE_BLANK_LINE)
             f.write(DockerfileSections.FOOTER)
             f.close()
-    
+
     def dump(self):
         return
 
 
 def args_handler():
-    parser = argparse.ArgumentParser(description='Build hardening_manifest.yaml for a given docker image, see: https://repo1.dso.mil/dsop/dccscr/-/blob/master/hardening%20manifest/README.md')
+    parser = argparse.ArgumentParser(
+        description="Build Dockerfile for a given docker image, see: "
+                    "https://repo1.dso.mil/dsop/dccscr/-/blob/master/hardening%20manifest/README.md")
     parser.add_argument('--docker_image_dir', help='The path to the docker image dir in the dockerfiles project',
                         required=True)
     parser.add_argument('--output_path', help='Full path of folder to output the hardening_manifest.yaml file',
@@ -49,8 +52,7 @@ def main():
     docker_image_dir = args.docker_image_dir
     output_path = args.output_path
 
-
-    print("Converting docker {1} to {2} ",)
+    print("Converting docker {0} to {1} ".format(docker_image_dir, output_path))
     dockerfile_ironbank = DockerfileIronBank(docker_image_dir, output_path)
     dockerfile_ironbank.build()
     dockerfile_ironbank.dump()
