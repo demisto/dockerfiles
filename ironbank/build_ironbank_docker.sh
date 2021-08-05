7d83ae4d473d80cc2192adc6e18062fa820b4659
Wissam Ghammashi <wghammashi@paloaltonetworks.com>
2021-08-05 18:57:22 +0300

    one more try to fix the git issues

--- ironbank/build_ironbank_docker.sh
+++ ironbank/build_ironbank_docker.sh
@@ -124,7 +124,9 @@ function commit_ironbank_image_to_repo_one {
   git fetch --all
   git branch
   git checkout development
-  git checkout -B $NEW_BRANCH_NAME origin/$NEW_BRANCH_NAME || git branch --set-upstream-to=origin/$NEW_BRANCH_NAME $NEW_BRANCH_NAME || git checkout -B $NEW_BRANCH_NAME
+  #git checkout -B $NEW_BRANCH_NAME origin/$NEW_BRANCH_NAME || git branch --set-upstream-to=origin/$NEW_BRANCH_NAME $NEW_BRANCH_NAME || git checkout -B $NEW_BRANCH_NAME
+  git checkout -B $NEW_BRANCH_NAME
+  git branch --set-upstream-to=origin/$NEW_BRANCH_NAME $NEW_BRANCH_NAME
   git pull
   cp -r $CURRENT_DIR/ironbank/$IMAGE_NAME/* .
   cp -r $CURRENT_DIR/docker/$IMAGE_NAME/requirements.txt .
