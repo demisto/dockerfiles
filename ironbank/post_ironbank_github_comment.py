43bc7b7bf14b14686fb109b3b744f31ae48fccdf
adaud <adaud@paloaltonetworks.com>
2021-10-27 12:08:41 +0300

    set verify to true

--- ironbank/post_ironbank_github_comment.py
+++ ironbank/post_ironbank_github_comment.py
@@ -37,7 +37,7 @@ def post_comment(image_commit_map):
         url = f'https://repo1.dso.mil/api/v4/projects/dsop%2Fopensource%2Fpalo-alto-networks%2Fdemisto%2F{image_name}/pipelines'
         try:
             print(f'Executing request to {url}')
-            res = requests.get(url=url, params=params, verify=False)
+            res = requests.get(url=url, params=params, verify=True)
             print(f'status code:{res.status_code}')
             print(f'content:{res.content}')
         except Exception as ex:
