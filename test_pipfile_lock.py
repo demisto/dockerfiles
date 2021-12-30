0d3c05328a77c9d512b8e0e132ce2d66607162a2
dschwartz <81086590+dorschw@users.noreply.github.com>
2021-12-30 18:06:17 +0200

    initial attempt, also updated pipfile&lock

---
+++ test_pipfile_lock.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from pipenv.project import Project
+
+PROHIBITED_FILES = {'requirements.txt'}
+REQUIRED_FILES = {'Dockerfile'}
+
+
+class DockerFileValidator:
+    def __init__(self):
+        self.project = Project()
+
+    def validate(self):
+        self._validate_files()
+        self._validate_pipfile_lock()
+
+    def _validate_pipfile_lock(self):
+        if self.project.pipfile_exists and not self.project.lockfile_exists:
+            raise ValueError("Missing Pipfile.lock\n"
+                             "Please run `pipenv lock` to generate it.")
+
+        if self.project.lockfile_exists and self.project.calculate_pipfile_hash() != self.project.get_lockfile_hash():
+            raise ValueError("Pipfile hash is different than the Pipfile.lock hash.\n"
+                             "Please run `pipenv lock` to update the Pipfile.lock")
+
+    @staticmethod
+    def _validate_files():
+        files = tuple(Path().glob('*'))
+        prohibited_but_exist = PROHIBITED_FILES.intersection(files)
+        required_but_missing = REQUIRED_FILES.difference(files)
+
+        errors = []
+        if prohibited_but_exist:
+            errors.append(f'The following files are not allowed: {",".join(prohibited_but_exist)}')
+            errors.append(f'The following files are required: {",".join(required_but_missing)}')
+
+        if errors:
+            raise ValueError("\n".join(errors))
+
+
+def main():
+    validator = DockerFileValidator()
+    validator.validate()  # raises on error
+
+
+if __name__ == '__main__':
+    main()
