d95c9eab894e1a373b9ef8b7ff806920b81c87ef
dschwartz <81086590+dorschw@users.noreply.github.com>
2022-01-02 15:06:38 +0200

    syntax fix

--- test_pipfile_lock.py
+++ test_pipfile_lock.py
@@ -1,4 +1,8 @@
+import os
+
+import re
 from pathlib import Path
+
 from pipenv.project import Project
 
 PROHIBITED_FILES = {'requirements.txt'}
@@ -6,12 +10,36 @@ REQUIRED_FILES = {'Dockerfile'}
 
 
 class DockerFileValidator:
-    def __init__(self):
+    def __init__(self, path: str = ''):
+        self.path = Path(path)
+        os.chdir(self.path)  # for debug purposes # todo remove
         self.project = Project()
+        self.docker_file = Path('Dockerfile')
 
     def validate(self):
+        # print("START ", "\t", str(self.path)) # todo remove
         self._validate_files()
         self._validate_pipfile_lock()
+        self._validate_dockerfile_pip_install()
+        # print("VALID ", "\t", str(self.path)) # todo remove
+
+    def _validate_dockerfile_pip_install(self):
+        dockerfile = self.docker_file.read_text() \
+            .replace("&&", "") \
+            .replace("\\", " ") \
+            .replace("\n", " ")
+
+        matches = tuple(re.finditer(r"pip install (?P<flags>-[\w\- .]+?) (?P<installed>[^-][\w+.]+)", dockerfile))
+
+        assert len(matches) == dockerfile.count("pip install"), 'Error parsing `pip install` commands'
+
+        for match in matches:
+            installed = match.group('installed')
+            if installed != 'requirements.txt':
+                raise ValueError(
+                    "pip install commands in the Dockerfile can only be used on requirements.txt"
+                    " (found {})".format(installed)
+                )
 
     def _validate_pipfile_lock(self):
         if self.project.pipfile_exists and not self.project.lockfile_exists:
@@ -22,18 +50,17 @@ class DockerFileValidator:
             raise ValueError("Pipfile hash is different than the Pipfile.lock hash.\n"
                              "Please run `pipenv lock` to update the Pipfile.lock")
 
-    @staticmethod
-    def _validate_files():
-        files = tuple(file.name for file in Path().glob('*'))
+    def _validate_files(self):
+        files = tuple(file.name for file in self.path.glob('*'))
         prohibited_but_exist = PROHIBITED_FILES.intersection(files)
         required_but_missing = REQUIRED_FILES.difference(files)
 
         errors = []
         if prohibited_but_exist:
-            errors.append(f'The following files are not allowed: ' + ",".join(prohibited_but_exist))
+            errors.append('The following files are not allowed: ' + ",".join(prohibited_but_exist))
 
         if required_but_missing:
-            errors.append(f'The following files are required: ' + ",".join(required_but_missing))
+            errors.append('The following files are required: ' + ",".join(required_but_missing))
 
         if errors:
             raise ValueError("\n".join(errors))
