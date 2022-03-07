f9831651f82f3454a4c901187d8d2cab00e08003
Bar Hochman <bhochman@paloaltonetworks.com>
2022-03-07 17:50:27 +0200

    python2 alternative

---
+++ docker/demisto-sdk/verify.py
@@ -0,0 +1,11 @@
+import subprocess
+
+python_version, _ = subprocess.Popen(['python3', '-V'], stdout=subprocess.PIPE).communicate()
+python_version = python_version.decode().strip('\n')
+assert '3' in python_version, f'No python3 in "python3" command {python_version=}'
+
+python_version, _ = subprocess.Popen(['python', '-V'], stdout=subprocess.PIPE).communicate()
+python_version = python_version.decode().strip('\n')
+assert '2.7' in python_version, f'No python2 in "python" command. {python_version=}'
+
+assert subprocess.Popen(['demisto-sdk', '--version']).wait() != 0, 'Could not run demisto-sdk'
