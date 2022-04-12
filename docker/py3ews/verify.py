372cf317c4306860aca99e1ac6f3e9b8fce9a586
israelpolishook <ipolishuk@paloaltonetworks.com>
2022-04-12 15:29:55 +0300

    commit

--- docker/py3ews/verify.py
+++ docker/py3ews/verify.py
@@ -13,16 +13,20 @@ from requests.exceptions import ConnectionError
 from collections import deque
 
 from multiprocessing import Process
-import exchangelib
-from exchangelib.errors import ErrorItemNotFound, ResponseMessageError, TransportError, RateLimitError, \
-    ErrorInvalidIdMalformed, \
-    ErrorFolderNotFound, ErrorMailboxStoreUnavailable, ErrorMailboxMoveInProgress, \
-    AutoDiscoverFailed, ErrorNameResolutionNoResults, ErrorInvalidPropertyRequest, ErrorIrresolvableConflict
-from exchangelib.items import Item, Message, Contact
-from exchangelib.services.common import EWSService, EWSAccountService
-from exchangelib.util import create_element, add_xml_child
-from exchangelib import IMPERSONATION, DELEGATE, Account, Credentials, \
-    EWSDateTime, EWSTimeZone, Configuration, NTLM, DIGEST, BASIC, FileAttachment, \
-    Version, Folder, HTMLBody, Body, Build, ItemAttachment
-from exchangelib.version import EXCHANGE_2007, EXCHANGE_2010, EXCHANGE_2010_SP2, EXCHANGE_2013, EXCHANGE_2016
-from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
+# import exchangelib
+# from exchangelib.errors import ErrorItemNotFound, ResponseMessageError, TransportError, RateLimitError, \
+#     ErrorInvalidIdMalformed, \
+#     ErrorFolderNotFound, ErrorMailboxStoreUnavailable, ErrorMailboxMoveInProgress, \
+#     AutoDiscoverFailed, ErrorNameResolutionNoResults, ErrorInvalidPropertyRequest, ErrorIrresolvableConflict
+# from exchangelib.items import Item, Message, Contact
+# from exchangelib.services.common import EWSService, EWSAccountService
+# from exchangelib.util import create_element, add_xml_child
+# from exchangelib import IMPERSONATION, DELEGATE, Account, Credentials, \
+#     EWSDateTime, EWSTimeZone, Configuration, NTLM, DIGEST, BASIC, FileAttachment, \
+#     Version, Folder, HTMLBody, Body, Build, ItemAttachment
+# from exchangelib.version import EXCHANGE_2007, EXCHANGE_2010, EXCHANGE_2010_SP2, EXCHANGE_2013, EXCHANGE_2016
+# from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
+import tzlocal
+
+test = tzlocal.get_localzone()
+print('all is good, `get_localzone() -> {}` is working'.format(test))
