168ace1c80a8aae779bf9f5d48ebbab3f72b894f
GuyAfik <guyafik11@gmail.com>
2022-07-04 19:15:34 +0300

    replace html2text with beautiful-soup

--- docker/crypto/verify.py
+++ docker/crypto/verify.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 import msal
-import html2text
+from bs4 import BeautifulSoup
 # Make sure cryptograph works
 key = Fernet.generate_key()
 print("All is good. cryptography generated a key: {}".format(key))
