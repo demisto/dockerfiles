c25d05a987f114c23e3d055da355b44f689cb1b5
ilan <ierukhimovic@paloaltonetworks.com>
2022-07-07 18:22:41 +0300

    no context

--- docker/content-dev/.p10k.zsh
+++ docker/content-dev/.p10k.zsh
@@ -80,7 +80,6 @@
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     toolbox                 # toolbox name (https://github.com/containers/toolbox)
-    context                 # user@hostname
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
