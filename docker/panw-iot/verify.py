71c57e84af02627fb09cd7f0389af88ec83d7423
Dawar <dhamid@paloaltonetworks.com>
2022-11-09 13:43:40 -0800

    adding panwiot dockerfiles

---
+++ docker/panw-iot/verify.py
@@ -0,0 +1,6 @@
+import pysnmp
+from oauthlib.oauth2 import BackendApplicationClient
+from requests_oauthlib import OAuth2Session
+import websocket as wsc
+
+print("All is good. PANW IoT python packages imported successfully")
