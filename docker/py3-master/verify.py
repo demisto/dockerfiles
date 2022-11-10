f437c60291d638a9593ff7c7773399db453dfcea
GuyAfik <guyafik11@gmail.com>
2022-11-10 15:40:10 +0200

    add mluphishing and genericsql

--- docker/py3-master/verify.py
+++ docker/py3-master/verify.py
@@ -137,3 +137,43 @@ print('nltk is good')
 import regex
 pattern = regex.Regex('\\\\d\\+', flags=regex.V0)
 print('regex is good')
+
+
+import numpy as np
+import pandas
+import sklearn
+from bs4 import BeautifulSoup
+import cv2 as cv
+import tldextract
+import dill
+import catboost
+from PIL import Image
+
+print('mlurlphishing is good')
+
+
+import sqlalchemy
+import psycopg2
+import pyodbc
+import psycopg2
+import cx_Oracle
+
+assert 'FreeTDS' in pyodbc.drivers()
+assert 'ODBC Driver 17 for SQL Server' in pyodbc.drivers()
+
+try:
+    # make sure oracle manages to load tns client libraries.
+    # Will fail but we want to be sure we don't fail on loading the driver
+    cx_Oracle.connect()
+except Exception as ex:
+    assert 'ORA-12162' in str(ex)
+
+
+# freetds test
+engine = sqlalchemy.create_engine('mssql+pyodbc:///testuser:testpass@127.0.0.1:1433/TEST?driver=FreeTDS')
+try:
+    engine.execute('select 1 as [Result]')
+except Exception as ex:
+    assert "Can't open lib" not in str(ex), "Failed because of missing lib: " + str(ex)
+
+print("genericsql is good")
