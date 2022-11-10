43fccd9eb72f174d79197b04d8efede94fd3f171
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2022-11-10 20:49:39 +0200

    commit to trigger build

--- docker/panw-iot/verify.py
+++ docker/panw-iot/verify.py
@@ -4,4 +4,4 @@ from requests_oauthlib import OAuth2Session
 import websocket as wsc
 from websocket import WebSocketApp
 
-print("All is good. PANW IoT python packages imported successfully")
+print("All is good. PANW IoT python packages imported successfully!")
