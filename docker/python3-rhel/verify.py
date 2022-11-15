e06ea0dd7b8ca43dba4e46ac775f0160579c1140
GuyAfik <guyafik11@gmail.com>
2022-11-15 12:53:08 +0200

    add verify.py and python dependancies

---
+++ docker/python3-rhel/verify.py
@@ -0,0 +1,15 @@
+import warnings
+import dateparser
+import tldextract
+# set warnings to throw an error
+warnings.simplefilter("error")
+d = dateparser.parse('1 day')
+extract = tldextract.TLDExtract(cache_dir=False,suffix_list_urls=None)
+t = extract('*test-test.com')
+print("all is good managed to parse: {}".format(d))
+
+# cve-2021-3177 See: https://bugs.python.org/issue42938
+# verify we are patched
+from ctypes import *
+print(c_double.from_param(1e300))
+print("all is good cve-2021-3177 is patched")
