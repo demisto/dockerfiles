ed58e054d9cd9b31952840d7380742c65e03ccb5
Judah Schwartz <juschwartz@paloaltonetworks.com>
2022-11-17 14:05:07 +0200

    a tag

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -283,10 +283,8 @@ EOF
           echo "adding latest tag"
           latest_tag="${DOCKER_ORG}/${image_name}:latest"
           docker tag "${image_full_name}" "${CR_REPO}/${latest_tag}"
-          docker push "${CR_REPO}/${latest_tag}" > /dev/null
-          echo "Done docker push for ${latest_tag}"
         fi
-        docker push ${CR_REPO}/${image_full_name} > /dev/null
+        docker push -a ${CR_REPO}/${image_full_name} > /dev/null
         echo "Done docker push for cr: ${image_full_name}"
     else
         echo "Skipping docker push for cr"
