76b0e5a05eaceefc6f4aa788d1f64905879d8505
Judah Schwartz <juschwartz@paloaltonetworks.com>
2022-11-17 13:43:23 +0200

    move repo

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -284,6 +284,7 @@ EOF
           latest_tag="${DOCKER_ORG}/${image_name}:latest"
           docker tag "${image_full_name}" "${latest_tag}"
           docker push "${CR_REPO}/${latest_tag}" > /dev/null
+          echo "Done docker push for ${latest_tag}"
         fi
         docker push ${CR_REPO}/${image_full_name} > /dev/null
         echo "Done docker push for cr: ${image_full_name}"
