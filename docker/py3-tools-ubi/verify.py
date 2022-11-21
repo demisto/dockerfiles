4f27b2519af3faf90c5feed5795c24e3f7165384
GuyAfik <guyafik11@gmail.com>
2022-11-21 11:17:04 +0200

    symlink verify.py to py3-tools

--- docker/py3-tools-ubi/verify.py
+++ docker/py3-tools-ubi/verify.py
@@ -1,17 +1,1 @@
-
-# geopy
-import geopy
-
-# pyjarm
-import jarm
-
-# paho-mqtt
-import paho.mqtt.client as mqtt
-mqttc = mqtt.Client()
-
-# tweepy
-import tweepy
-
-print("All good, and you look amazing.")
-
-
+../py3-tools/verify.py
