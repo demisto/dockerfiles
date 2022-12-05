c808aa7c7f59f7d75b724cef1656606232b02ce5
GuyAfik <guyafik11@gmail.com>
2022-12-06 00:25:51 +0200

    update unrar subprocess command to query version

--- docker/unzip/verify.py
+++ docker/unzip/verify.py
@@ -1,4 +1,4 @@
 import subprocess
-subprocess.check_output(["unrar", "--version"], text=True)
+subprocess.check_output(["unrar", "-V"], text=True)
 subprocess.check_output(["7z"], text=True)
 print('unzip is good!!!')
