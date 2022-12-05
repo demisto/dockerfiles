b787599fb89630d5bbef5d8804a4e4fb6d6e3afe
GuyAfik <guyafik11@gmail.com>
2022-12-05 19:15:40 +0200

    add verify.py to missing docker images + add symlinks

---
+++ docker/slackv3/verify.py
@@ -0,0 +1,9 @@
+import slack_sdk
+from slack_sdk.errors import SlackApiError
+from slack_sdk.socket_mode.aiohttp import SocketModeClient
+from slack_sdk.socket_mode.request import SocketModeRequest
+from slack_sdk.socket_mode.response import SocketModeResponse
+from slack_sdk.web.async_client import AsyncWebClient
+from slack_sdk.web.async_slack_response import AsyncSlackResponse
+from slack_sdk.web.slack_response import SlackResponse
+print('slackv3 is good!!!')
