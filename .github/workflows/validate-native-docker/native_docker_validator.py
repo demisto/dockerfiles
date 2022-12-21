49ac109611c18f466e8c26e0d6cd4bf128fe276e
YuvHayun <yhayun@paloaltonetworks.com>
2022-12-21 18:20:32 +0200

    testing

--- .github/workflows/validate-native-docker/native_docker_validator.py
+++ .github/workflows/validate-native-docker/native_docker_validator.py
@@ -6,20 +6,17 @@ from typing import Iterable
 
 def main():
     parser = argparse.ArgumentParser(description='Deploy a pack from a contribution PR to a branch')
-    parser.add_argument('-l', '--labels', help='list of the PR labels')
     parser.add_argument('-c', '--changed_files', help='list of the changed files')
     args = parser.parse_args()
 
-    labels = args.labels
     changed_files = args.changed_files.split(";")
-    validate_native_docker_image(labels, changed_files)
+    validate_native_docker_image(changed_files)
 
 
-def validate_native_docker_image(labels, changed_files):
+def validate_native_docker_image(changed_files):
     if is_docker_being_updated(changed_files):
         # if is_update_related_to_native_dcoker()
-        #     if not 'docker update approved' in labels:
-        #         return False
+        #     return False
         return False
     return True
 
