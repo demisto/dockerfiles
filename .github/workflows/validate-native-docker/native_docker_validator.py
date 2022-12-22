25bcb6e0e880c106a6d3099a220a847e384c0755
YuvHayun <yhayun@paloaltonetworks.com>
2022-12-22 15:28:30 +0200

    in prog

--- .github/workflows/validate-native-docker/native_docker_validator.py
+++ .github/workflows/validate-native-docker/native_docker_validator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import argparse
-from typing import Iterable
 
+
+DOCKER_FILES_SUFFIX = ["Dockerfle", "Pipfile", "Pipfile.lock"]
 
 
 def main():
@@ -9,23 +10,24 @@ def main():
     parser.add_argument('-c', '--changed_files', help='list of the changed files')
     args = parser.parse_args()
     changed_files = args.changed_files
-    print(changed_files)
-    # changed_files = args.changed_files.split(";")
+    changed_files = args.changed_files.split(" ")
     
     validate_native_docker_image(changed_files)
 
 
 def validate_native_docker_image(changed_files):
     if is_docker_being_updated(changed_files):
-        # if is_update_related_to_native_dcoker()
-        #     return False
-        return False
+        if is_update_related_to_native_dcoker()
+            return False
     return True
 
 
 def is_docker_being_updated(changed_files):
     for file in changed_files:
-        print(file) 
+        for suffix in DOCKER_FILES_SUFFIX:
+            if file.endswith(suffix):
+                return True
+    return False
 
 
 if __name__ == '__main__':
