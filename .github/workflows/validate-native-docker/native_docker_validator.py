a327d4c1f4b249c8c5683e4401c187fd59e83ceb
YuvHayun <yhayun@paloaltonetworks.com>
2022-12-23 12:34:05 +0200

    tests

--- .github/workflows/validate-native-docker/native_docker_validator.py
+++ .github/workflows/validate-native-docker/native_docker_validator.py
@@ -20,8 +20,8 @@ def validate_native_docker_image(changed_files):
     if is_docker_being_updated(changed_files):
         response = requests.get("https://raw.githubusercontent.com/demisto/content/master/Tests/docker_native_image_config.json")
         print(response)
-        if is_update_related_to_native_docker(changed_files):
-            return False
+        # if is_update_related_to_native_docker(changed_files):
+        #     return False
     return True
 
 
@@ -33,19 +33,19 @@ def is_docker_being_updated(changed_files):
     return False
 
 
-def is_update_related_to_native_docker(changed_files):
-    dockers = []
-    for file in changed_files:
-        for suffix in DOCKER_FILES_SUFFIX:
-            if file.endswith(suffix):
+# def is_update_related_to_native_docker(changed_files):
+#     dockers = []
+#     for file in changed_files:
+#         for suffix in DOCKER_FILES_SUFFIX:
+#             if file.endswith(suffix):
                 
 
-def get_supported_dockers():
-    supported_docker_images_dict = {}.get("native_images")
-    supported_dockers_ls = []
-    for native_docker in  supported_docker_images_dict.values():
-        supported_dockers_ls.extend(native_docker.get("supported_docker_images", []))
-    return supported_dockers_ls
+# def get_supported_dockers():
+#     supported_docker_images_dict = {}.get("native_images")
+#     supported_dockers_ls = []
+#     for native_docker in  supported_docker_images_dict.values():
+#         supported_dockers_ls.extend(native_docker.get("supported_docker_images", []))
+#     return supported_dockers_ls
 
 if __name__ == '__main__':
     main()
