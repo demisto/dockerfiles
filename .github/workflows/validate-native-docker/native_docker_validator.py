da3e1cb48c7a7b537ec85218a4ed6fceeb4aae35
YuvHayun <yhayun@paloaltonetworks.com>
2022-12-23 12:32:17 +0200

    tests

--- .github/workflows/validate-native-docker/native_docker_validator.py
+++ .github/workflows/validate-native-docker/native_docker_validator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import requests
 
 
 DOCKER_FILES_SUFFIX = ["Dockerfle", "Pipfile", "Pipfile.lock"]
@@ -17,7 +18,9 @@ def main():
 
 def validate_native_docker_image(changed_files):
     if is_docker_being_updated(changed_files):
-        if is_update_related_to_native_dcoker()
+        response = requests.get("https://raw.githubusercontent.com/demisto/content/master/Tests/docker_native_image_config.json")
+        print(response)
+        if is_update_related_to_native_docker(changed_files):
             return False
     return True
 
@@ -29,6 +32,20 @@ def is_docker_being_updated(changed_files):
                 return True
     return False
 
+
+def is_update_related_to_native_docker(changed_files):
+    dockers = []
+    for file in changed_files:
+        for suffix in DOCKER_FILES_SUFFIX:
+            if file.endswith(suffix):
+                
+
+def get_supported_dockers():
+    supported_docker_images_dict = {}.get("native_images")
+    supported_dockers_ls = []
+    for native_docker in  supported_docker_images_dict.values():
+        supported_dockers_ls.extend(native_docker.get("supported_docker_images", []))
+    return supported_dockers_ls
 
 if __name__ == '__main__':
     main()
