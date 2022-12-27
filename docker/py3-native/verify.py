7b323b57a371f2b74db931ba920b6c2e5732f1b6
Danny_Fried <dfried@paloaltonetworks.com>
2022-12-27 14:09:16 +0200

    Validate OpenSSL version in native image.

--- docker/py3-native/verify.py
+++ docker/py3-native/verify.py
@@ -2,3 +2,12 @@
 import regex
 pattern = regex.Regex('\\\\d\\+', flags=regex.V0)
 print('regex is good')
+
+import ssl
+if ssl.OPENSSL_VERSION_INFO >= (3,0,0,0,0):
+    print ('openSSL version changed.\
+           Please validate SSL Legacy renegotiation error is not an issue.\
+           If SSL Legacy renegotiation error occurs, please refer to python3 image Dockerfile for reference.')    
+    exit(1)
+print('openSSL version is good.')
+    
