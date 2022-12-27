331e109ce0afb2c425b8a09b189aebd5ec97625b
Danny_Fried <dfried@paloaltonetworks.com>
2022-12-27 14:20:34 +0200

    Format print.

--- docker/py3-native/verify.py
+++ docker/py3-native/verify.py
@@ -5,9 +5,9 @@ print('regex is good')
 
 import ssl
 if ssl.OPENSSL_VERSION_INFO >= (3,0,0,0,0):
-    print ('openSSL version changed.\
-           Please validate SSL Legacy renegotiation error is not an issue.\
-           If SSL Legacy renegotiation error occurs, please refer to python3 image Dockerfile for reference.')    
+    print ('openSSL version changed.\n\
+Please validate SSL Legacy renegotiation error is not an issue.\n\
+If SSL Legacy renegotiation error occurs, please refer to python3 image Dockerfile for reference.')
     exit(1)
 print('openSSL version is good.')
     
