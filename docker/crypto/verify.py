e024bc0c6f506cf12c67b7cc83e1c8d86304ba69
ShacharKidor <skidorball@paloaltonetworks.com>
2023-01-18 17:43:44 +0200

    Added test to make sure MD4 is enabled - to verify.py

--- docker/crypto/verify.py
+++ docker/crypto/verify.py
@@ -3,5 +3,11 @@ import msal
 from bs4 import BeautifulSoup
 # Make sure cryptograph works
 key = Fernet.generate_key()
+
+# Make sure MD4 is enabled:
+import hashlib
+hashlib.algorithms_available
+assert 'md4' in hashlib.algorithms_available
+
 print("All is good. cryptography generated a key: {}".format(key))
 print(msal.oauth2cli.assertion.JwtAssertionCreator('', 'All is good. msal was imported successfully').algorithm)
