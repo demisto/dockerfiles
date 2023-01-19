18132547ef69252a5f401030955d933d732163f0
ShacharKidor <skidorball@paloaltonetworks.com>
2023-01-19 09:32:56 +0200

    - Added assert to py3ews verify.py - Reverted the change in crypto verify.py

--- docker/crypto/verify.py
+++ docker/crypto/verify.py
@@ -1,14 +1,7 @@
 from cryptography.fernet import Fernet
 import msal
-import hashlib
 from bs4 import BeautifulSoup
 # Make sure cryptograph works
 key = Fernet.generate_key()
-
-# Make sure MD4 is enabled:
-hashlib.algorithms_available
-assert 'md4' in hashlib.algorithms_available
-hashlib.new('md4', b"text")
-
 print("All is good. cryptography generated a key: {}".format(key))
 print(msal.oauth2cli.assertion.JwtAssertionCreator('', 'All is good. msal was imported successfully').algorithm)
