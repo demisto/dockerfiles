a434de0d9f6f296cbf8459902cd5cc8e2740ab6f
GuyAfik <guyafik11@gmail.com>
2023-06-27 13:34:53 +0300

    try to add chmod +r

--- docker/py3-native/chromium/download_chromedriver.sh
+++ docker/py3-native/chromium/download_chromedriver.sh
@@ -7,7 +7,7 @@ set -e
 # Script to download relevant chromedriver according to installed chrome version
 ###########################
 
-# for testing pruposes (outside of the container) you can set GOOGLE_CHROME_VERSION env var
+# for testing purposes (outside of the container) you can set GOOGLE_CHROME_VERSION env var
 # for example: 
 # docker run --rm -it -v `pwd`:/work -w /work -e GOOGLE_CHROME_VERSION=91.0.4472 demisto/chromium:1.0.0.23161 docker/chromium/download_chromedriver.sh
 if [ -z "$GOOGLE_CHROME_VERSION" ]; then
