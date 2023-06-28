b1ffac424631c94e2d9e8f83ae6e9ef9ef16cbb0
dmeylakh <dmeylakh@paloaltonetworks.com>
2023-06-28 12:54:20 +0300

    comitting dockerbuild

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -103,7 +103,7 @@ function commit_dockerfiles_trust {
     if [[ $(git status --short) ]]; then
         echo "dockerfiles-trust: found modified/new files to commit"
         git status --short
-        git pull
+        git pull -X ours --rebase --autostash
         echo "starting commit loop..."
         git add .
         git commit -m "`date`: trust update from PR: ${CIRCLE_PULL_REQUEST}"
