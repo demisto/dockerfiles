2fde3b99916d943df8295b69d3cd796a620bec26
dansterenson <dansterenson@gmail.com>
2023-07-13 16:57:25 +0300

    fix verify.py

--- docker/stringsifter/verify.py
+++ docker/stringsifter/verify.py
@@ -1,4 +1,12 @@
 from subprocess import Popen, PIPE
 
-Popen(["flarestrings", 'requirements.txt'], stdout=PIPE)
-print("All OK. StringSifter imported successfully")
+command = "flarestrings requirements.txt"
+
+process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+
+return_code = process.returncode
+if return_code != 0:
+    raise Exception("verification failed")
+
+print("Successfully executed stringsifter command")
