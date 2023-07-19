79b71a757ae43567223acabe8d261e6825e7e350
ashamah <ashamah@paloaltonetworks.com>
2023-07-19 09:15:11 +0300

    Add Huggingface Docker image

---
+++ docker/huggingface/download_model.py
@@ -0,0 +1,13 @@
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer
+
+model_names = ['distilbert-base-cased-distilled-squad']
+
+
+def main():
+    for model in model_names:
+        AutoModelForQuestionAnswering.from_pretrained(model)
+        AutoTokenizer.from_pretrained(model)
+
+
+if __name__ == "__main__":
+    main()
