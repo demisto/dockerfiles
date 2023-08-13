f956a6c2dbb60bb679fccf259443fd7748eb3973
ilan <ierukhimovic@paloaltonetworks.com>
2023-08-13 13:05:34 +0300

    remove comment

--- docker/websockets/verify.py
+++ docker/websockets/verify.py
@@ -1,8 +1,6 @@
 import websockets
 from websockets.sync.client import connect
 
-
-# write a script to test that websockets work properly
 with connect("ws://echo.websocket.org") as connection:
     echo = connection.recv()
     print(echo)
