8b5a69aa5baf1577352e88a970e545b01a45e7f3
sapirshuker <sshuker@paloaltonetworks.com>
2023-08-16 09:58:21 +0300

    update pipfile python version, change branch name

--- utils/auto_dockerfile_update/update_dockerfiles.py
+++ utils/auto_dockerfile_update/update_dockerfiles.py
@@ -235,7 +235,7 @@ def update_external_base_dockerfiles(git_repo: Repo, no_timestamp_updates=True) 
         latest_tag_last_updated = latest_tag.get('last_updated', '')
 
         if is_docker_file_outdated(file, latest_tag_name, latest_tag_last_updated, no_timestamp_updates):
-            branch_name = fr"TEST13auTESTtoTESTupdate/Update_{file['repo']}_{file['image_name']}_from_{file['tag']}_to_{latest_tag_name}"
+            branch_name = fr"TEST15auTESTtoTESTupdate/Update_{file['repo']}_{file['image_name']}_from_{file['tag']}_to_{latest_tag_name}"
             update_and_push_dockerfiles(git_repo, branch_name, [file], latest_tag_name)
             print(f"Updated {file['path']}")
     print("Finished to update dockerfiles")
@@ -284,7 +284,7 @@ def update_internal_base_dockerfile(git_repo: Repo) -> None:
         for batch_slice in batch(outdated_files, BATCH_SIZE):
             if batch_slice[0]["name"] == "ansible-runner" or batch_slice[0]["name"] == "parse-emails":
                 image_names = reduce(lambda a, b: f"{a}-{b}", [file['name'] for file in batch_slice])
-                branch_name = fr"TEST13auTESTtoTESTupdate/{base_image}_{image_names}_{latest_tag_name}"
+                branch_name = fr"TEST15auTESTtoTESTupdate/{base_image}_{image_names}_{latest_tag_name}"
                 update_and_push_dockerfiles(git_repo, branch_name, batch_slice, latest_tag_name)
     print("Finished to update dockerfiles")
 
