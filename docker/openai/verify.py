c9e76412dfc051e2db62814f13d774b3c8810808
Anas Yousef <44998563+anas-yousef@users.noreply.github.com>
2023-08-17 15:02:22 +0300

    Updated verify.py

--- docker/openai/verify.py
+++ docker/openai/verify.py
@@ -1,10 +1,10 @@
 import openai
 import langchain
 import tiktoken
+import faiss
 import pdf2image
 import pypdf
 import unstructured
 import layoutparser
-import faiss
 
 print('openai is good!!!')
