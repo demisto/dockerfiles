5c33532577cd41b5f146f2a172dcc369a3dfdda7
RotemAmit <ramit@paloaltonetworks.com>
2023-10-26 17:16:56 +0300

    added the new github action

---
+++ .github/workflows/handle-new-contrib/handle_new_contribution.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Deploy a pack from a contribution PR to a branch')
+    parser.add_argument('-c', '--$content_roles', help='A dictionary that contains the content_roles')
+    args = parser.parse_args()
+    content_roles = args.changed_files
+    print(content_roles)
+    save_contrib_tl(content_roles)
+
+
+def save_contrib_tl(content_roles):
+    contrib_tl_username = content_roles.get("CONTRIBUTION_TL")
+    if not contrib_tl_username:
+        raise Exception("There isn't a contribution TL in .github/content_roles.json")
+    # save the contrib_tl username to a file for a later use in the workflow
+    with open("contrib_tl.txt", "w") as f:
+        f.write(contrib_tl_username)
+
+
+if __name__ == '__main__':
+    main()
