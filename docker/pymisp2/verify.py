a623ef820360713ac06d174f7e3f55dcbc642bff
Judah Schwartz <juschwartz@paloaltonetworks.com>
2023-11-15 11:35:07 +0200

    lock

--- docker/pymisp2/verify.py
+++ docker/pymisp2/verify.py
@@ -2,3 +2,9 @@ from pymisp.tools import EMailObject, GenericObjectGenerator
 from pymisp import ExpandedPyMISP, PyMISPError, MISPObject, MISPSighting, MISPEvent, MISPAttribute
 
 print("All is good. pymisp imported successfully")
+def hello(para,):
+    """
+
+    :param para:
+    :return:
+    """
