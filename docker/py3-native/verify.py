fe871a97be3fc151f93702bdd3524b84d51419c5
tkatzir <tkatzir@paloaltonetworks.com>
2023-11-21 13:27:23 +0200

    Add pychrome to native

--- docker/py3-native/verify.py
+++ docker/py3-native/verify.py
@@ -7,3 +7,6 @@ print('regex is good')
 # make sure that the iproute command is installed in py3-native image
 import subprocess
 subprocess.check_output(["ip", "route", "list"], text=True, stderr=subprocess.STDOUT)
+
+import pychrome
+print(f'Using pychrome version {pychrome.__version__}')
