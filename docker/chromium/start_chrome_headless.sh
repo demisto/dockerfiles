c285acc02f56ac324a614f8757ef63ce1c933180
tkatzir <tkatzir@paloaltonetworks.com>
2023-11-29 17:26:24 +0200

    Only start chrome

--- docker/chromium/start_chrome_headless.sh
+++ docker/chromium/start_chrome_headless.sh
@@ -1,27 +1,69 @@
 #!/bin/bash
 
+# # Default values (consider injecting them from the outside as arguments)
+# ignore_certificate_errors=true
+# chrome_binary="/opt/google/chrome/google-chrome"
+# remote_debugging_port=9222
+# max_attempts=3
+# user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+
+
+# start_chrome() {
+#     local cert_errors_option=""
+#     if [ "$ignore_certificate_errors" = true ]; then
+#         cert_errors_option="--ignore-certificate-errors"
+#     fi
+
+#     echo "Starting Chrome..."
+#     if ! nohup "$chrome_binary" --headless --disable-gpu --no-sandbox --hide-scrollbars --disable-infobars --start-maximized --start-fullscreen $cert_errors_option --disable-dev-shm-usage --user-agent="\"$user_agent\"" --remote-debugging-port="$remote_debugging_port" & disown; then
+#         echo "Failed to start Chrome."
+#         return 1
+#     fi
+#     return 0
+# }
+
+
+# while [[ "$#" -gt 0 ]]; do
+#     case "${1}" in
+#         --validate-certificates) ignore_certificate_errors=false ;;
+#         --chrome-binary) chrome_binary="${2}"; shift ;;
+#         --user-agent) user_agent="${2}"; shift ;;
+#         --port) remote_debugging_port="${2}"; shift ;;
+#         *) echo "Unknown option: ${1}" ;;
+#     esac
+#     shift
+# done
+
+
+# # Retry loop to start up chrome
+# for attempt in $(seq 1 $max_attempts); do
+#     echo "Attempt $attempt of $max_attempts"
+
+#     # Check if Chrome is already running on the specified port
+#     if ps -aux | grep "chrome" | grep "headless" | grep "9222"; then
+#         echo "Chrome is already running on port $remote_debugging_port."
+#         exit 0
+#     fi
+
+#     # Try to start Chrome
+#     if start_chrome; then
+#         echo "Chrome started successfully."
+#         exit 0
+#     fi
+
+#     sleep 3
+# done
+
+# echo "Failed to start Chrome after $max_attempts attempts."
+# exit 1
+
+
 # Default values (consider injecting them from the outside as arguments)
 ignore_certificate_errors=true
 chrome_binary="/opt/google/chrome/google-chrome"
 remote_debugging_port=9222
 max_attempts=3
 user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
-
-
-start_chrome() {
-    local cert_errors_option=""
-    if [ "$ignore_certificate_errors" = true ]; then
-        cert_errors_option="--ignore-certificate-errors"
-    fi
-
-    echo "Starting Chrome..."
-    if ! nohup "$chrome_binary" --headless --disable-gpu --no-sandbox --hide-scrollbars --disable-infobars --start-maximized --start-fullscreen $cert_errors_option --disable-dev-shm-usage --user-agent="\"$user_agent\"" --remote-debugging-port="$remote_debugging_port" & disown; then
-        echo "Failed to start Chrome."
-        return 1
-    fi
-    return 0
-}
-
 
 while [[ "$#" -gt 0 ]]; do
     case "${1}" in
@@ -34,25 +76,10 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
+local cert_errors_option=""
+if [ "$ignore_certificate_errors" = true ]; then
+    cert_errors_option="--ignore-certificate-errors"
+fi
 
-# Retry loop to start up chrome
-for attempt in $(seq 1 $max_attempts); do
-    echo "Attempt $attempt of $max_attempts"
-
-    # Check if Chrome is already running on the specified port
-    if ps -aux | grep "chrome" | grep "headless" | grep "9222"; then
-        echo "Chrome is already running on port $remote_debugging_port."
-        exit 0
-    fi
-
-    # Try to start Chrome
-    if start_chrome; then
-        echo "Chrome started successfully."
-        exit 0
-    fi
-
-    sleep 3
-done
-
-echo "Failed to start Chrome after $max_attempts attempts."
-exit 1
+echo "Starting Chrome..."
+nohup "$chrome_binary" --headless --disable-gpu --no-sandbox --hide-scrollbars --disable-infobars --start-maximized --start-fullscreen $cert_errors_option --disable-dev-shm-usage --user-agent="\"$user_agent\"" --remote-debugging-port="$remote_debugging_port" & disown
