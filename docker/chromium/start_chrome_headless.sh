ed1aeb82431bd97db480f47c4920d069b64ae42f
tkatzir <tkatzir@paloaltonetworks.com>
2023-12-13 11:46:49 +0200

    Chrome User Options (#22973)

--- docker/chromium/start_chrome_headless.sh
+++ docker/chromium/start_chrome_headless.sh
@@ -6,6 +6,7 @@ chrome_binary="/opt/google/chrome/google-chrome"
 remote_debugging_port=9222
 max_attempts=3
 user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"
+user_options=""
 
 while [[ "$#" -gt 0 ]]; do
     case "${1}" in
@@ -13,6 +14,7 @@ while [[ "$#" -gt 0 ]]; do
         --chrome-binary) chrome_binary="${2}"; shift ;;
         --user-agent) user_agent="${2}"; shift ;;
         --port) remote_debugging_port="${2}"; shift ;;
+        --user-options) user_options="${2}"; shift ;;
         *) echo "Unknown option: ${1}" ;;
     esac
     shift
@@ -24,4 +26,4 @@ if [ "$ignore_certificate_errors" = true ]; then
 fi
 
 echo "Starting Chrome..."
-nohup "$chrome_binary" --headless --disable-gpu --no-sandbox --hide-scrollbars --disable-infobars --start-maximized --start-fullscreen $cert_errors_option --disable-dev-shm-usage --user-agent="\"$user_agent\"" --remote-debugging-port="$remote_debugging_port" & disown
+nohup "$chrome_binary" --headless --disable-gpu --no-sandbox --hide-scrollbars --disable-infobars --start-maximized --start-fullscreen $cert_errors_option --disable-dev-shm-usage --user-agent="\"$user_agent\"" "$user_options --remote-debugging-port=$remote_debugging_port" & disown
