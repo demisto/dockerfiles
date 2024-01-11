c4fbee439df83bcb0001651e7a7462d945d4078b
Judah Schwartz <juschwartz@paloaltonetworks.com>
2024-01-11 11:42:45 +0200

    docstrings and test

--- utils/validate_deprecated_dockers_json.py
+++ utils/validate_deprecated_dockers_json.py
@@ -6,15 +6,20 @@ import json
 from datetime import datetime
 
 
-def parse_file(f):
+def parse_config_contents(file_contents: str):
     return {
         line.split("=")[0].strip(): line.split("=")[1].strip()
-        for line in f.readlines()
-        if not line.isspace() and "#" not in line
+        for line in file_contents.split('\n')
+        if line and not line.isspace() and "#" not in line
     }
 
 
-def get_entry(image_name, previous_json) -> Optional[dict]:
+def get_entry_by_name(image_name, previous_json) -> Optional[dict]:
+    """
+    Given a deprecatedJson config, return the proper config for a given image name
+    :param image_name: the image name to look up
+    :param previous_json: the pre-existing file, used for preserving the entered time
+    """
     matches = [entry for entry in previous_json if entry["image_name"] == image_name]
     match len(matches):
         case 0:
@@ -25,23 +30,20 @@ def get_entry(image_name, previous_json) -> Optional[dict]:
             raise ValueError(f"More than 1 entry exits for image {image_name}")
 
 
-def compare_deprecated_images(deprecated_json_file, deprecated_json_from_configs):
-    names_in_file = {i["image_name"] for i in deprecated_json_file}
-    names_in_configs = {i["image_name"] for i in deprecated_json_from_configs}
-
-    names_should_be_in_file = names_in_configs - names_in_file
-    names_should_be_in_config = names_in_file - names_in_configs
-    if names_should_be_in_file or names_should_be_in_config:
-        raise ValueError(
-            f"The deprecated_images.json file is not valid. Did you deprecate an image in this pr? Add it to docker/deprecated_images.json. You can use util/validate_deprecated_dockers_json.py --fix to do so."
-            f"Set of items that are deprecated according to their build.conf but not in the deprecated_images.json: {names_should_be_in_file}\n"
-            f"Set of items that are in the file but not deprecated according to the build.conf {names_should_be_in_config}"
-        )
+def compare_deprecated_images(deprecated_json_1, deprecated_json_2):
+    """
+    Given two versions of deprecated_json file (newly-made and preexisting) will return image names only in each list
+    """
+    names_in_1 = {i["image_name"] for i in deprecated_json_1}
+    names_in_2 = {i["image_name"] for i in deprecated_json_2}
+    return names_in_1 - names_in_2, names_in_2 - names_in_1
 
 
 def main():
     arg_parser = argparse.ArgumentParser(
-        description="Generate json for deprecated images."
+        description="""Builds a new version of the deprecated json file based on the build.conf files
+If in validate mode - will fail if the file is not a match to the existing deprecated.json file
+If in fix more - will override the file with the correct version based on the build.conf files"""
     )
     arg_parser.add_argument("--exclude", default="")
     arg_parser.add_argument(
@@ -64,23 +66,23 @@ def main():
             conf_path = path / "build.conf"
             if not conf_path.is_file():
                 continue
-            with open(conf_path) as f:
-                config = parse_file(f)
-                if config.get("deprecated", False):
-                    image_name = f"demisto/{path.name}"
-                    prev_entry = get_entry(image_name, previous_json)
-                    created_time = (
-                        prev_entry["created_time_utc"]
-                        if prev_entry
-                        else str(datetime.now())
-                    )
-                    deprecated_info.append(
-                        {
-                            "image_name": image_name,
-                            "reason": config.get("deprecated_reason", ""),
-                            "created_time_utc": created_time,
-                        }
-                    )
+            config_contents = Path(conf_path).read_text()
+            config = parse_config_contents(config_contents)
+            if config.get("deprecated", False):
+                image_name = f"demisto/{path.name}"
+                prev_entry = get_entry_by_name(image_name, previous_json)
+                created_time = (
+                    prev_entry["created_time_utc"]
+                    if prev_entry
+                    else str(datetime.now())
+                ) # if already in the config take the time entered from previous version
+                deprecated_info.append(
+                    {
+                        "image_name": image_name,
+                        "reason": config.get("deprecated_reason", ""),
+                        "created_time_utc": created_time,
+                    }
+                )
     if args.fix:
         with open(args.deprecated_path, "w") as f:
             json.dump(
@@ -91,7 +93,13 @@ def main():
             )
 
     else:
-        compare_deprecated_images(previous_json, deprecated_info)
+        names_should_be_in_file, names_should_be_in_config = compare_deprecated_images(previous_json, deprecated_info)
+        if names_should_be_in_file or names_should_be_in_config:
+            raise ValueError(
+                f"The deprecated_images.json file is not valid. Did you deprecate an image in this pr? Add it to docker/deprecated_images.json. You can use util/validate_deprecated_dockers_json.py --fix to do so."
+                f"Set of items that are deprecated according to their build.conf but not in the deprecated_images.json: {names_should_be_in_file}\n"
+                f"Set of items that are in the file but not deprecated according to the build.conf {names_should_be_in_config}"
+            )
         print("deprecated_images.json file is valid")
 
 
