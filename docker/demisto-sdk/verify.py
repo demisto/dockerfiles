a752130819c21da6531e4e7f47eac118f32d7a8d
GuyAfik <guyafik11@gmail.com>
2024-03-07 09:29:05 +0200

    run mdx_server_is_up

--- docker/demisto-sdk/verify.py
+++ docker/demisto-sdk/verify.py
@@ -1,4 +1,7 @@
 from demisto_sdk.commands.common.MDXServer import start_local_MDX_server
+from demisto_sdk.commands.common.hook_validations.readme import mdx_server_is_up
 
 with start_local_MDX_server():
-    print("successfully started mdx server")
+    assert mdx_server_is_up()
+
+print("successfully started mdx server")
