8108b47967dc5859c9b1ae0367dd2b9e8af9372d
sapirshuker <sshuker@paloaltonetworks.com>
2024-03-26 09:52:26 +0200

    update verify script

--- docker/flask-nginx/verify.py
+++ docker/flask-nginx/verify.py
@@ -1,4 +1,5 @@
 import subprocess
+from taxii2client.v21 import Server
 
 res = subprocess.check_output(['nginx', '-V'], text=True, stderr=subprocess.STDOUT)
 print(res)
@@ -8,3 +9,6 @@ assert 'nginx/1.' in res
 # verify that nginx test passes
 res = subprocess.check_output(['nginx', '-t'], text=True, stderr=subprocess.STDOUT)
 print(res)
+
+server = Server('https://example.com/taxii2/', user='user_id', password='user_password')
+print(server.title)
