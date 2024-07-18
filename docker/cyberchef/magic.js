91f880b65e731f759ccaa1a6643604b2b60bdcdd
Nik Stuckenbrock <privat@nikstuckenbrock.de>
2024-07-18 08:53:14 +0200

    Update scripts with default value and add magic

---
+++ docker/cyberchef/magic.js
@@ -0,0 +1,17 @@
+const cyberchefNode = require("cyberchef-node");
+
+async function runMagic(input, args = {}) {
+    const parsedArgs = JSON.parse(args);
+
+    try {
+        const result = await cyberchefNode.magic(input, parsedArgs);
+        console.log(result);
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+const input = process.argv[2];
+const args = process.argv[3];
+runMagic(input, args);
