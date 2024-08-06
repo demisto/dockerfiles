fb17c05ec4b8ba060f9a2d222aedfa70921f15fc
Ni-Knight <davrahami@paloaltonetworks.com>
2024-08-06 16:20:09 +0300

    verify fix

--- docker/pysigma/verify.py
+++ docker/pysigma/verify.py
@@ -3,7 +3,7 @@ from sigma.backends.cortexxdr import CortexXDRBackend
 from sigma.backends.splunk import SplunkBackend
 from sigma.backends.sentinelone import SentinelOneBackend
 from sigma.backends.qradar import QradarBackend
-from sigma.backends.qradar import Microsoft365DefenderBackend
-from sigma.backends.qradar import CarbonBlackBackend
+from sigma.backends.microsoft365defender import Microsoft365DefenderBackend
+from sigma.backends.carbonblack import CarbonBlackBackend
 
 print('All good, pysigma and backends loaded successfully')
