2a862e1a33e8b1afcd1895dd9c11b5f932d79a67
dawar-zingbox <dhamid@paloaltonetworks.com>
2024-10-01 09:29:37 -0700

    adding dnspython (#32868)

--- docker/panw-iot/verify.py
+++ docker/panw-iot/verify.py
@@ -6,7 +6,30 @@ from websocket import WebSocketApp
 import snap7
 import pandas
 import orionsdk
+import dns.resolver
+
+def lookup_dns_record(domain, record_type='A'):
+    """
+    Performs a DNS lookup for the specified domain and record type.
+
+    Args:
+        domain (str): The domain to look up.
+        record_type (str, optional): The type of DNS record to look up (e.g., 'A', 'MX', 'TXT'). Default is 'A'.
+
+    Returns:
+        list: A list of DNS records for the specified domain and record type.
+    """
+    try:
+        resolver = dns.resolver.Resolver()
+        answers = resolver.resolve(domain, record_type)
+        return [str(record) for record in answers]
+    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.exception.DNSException) as e:
+        return []
+    except Exception as e:
+        return []
+
 
 snap7client = snap7.client.Client()
 swis = orionsdk.SwisClient("server", "username", "password")
+lookup = lookup_dns_record("google.com")
 print("All is good. PANW IoT python packages imported successfully!")
