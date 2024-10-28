bac54ea66443ee36749ce906fc4f968194729117
ShacharKidor <skidorball@paloaltonetworks.com>
2024-10-28 10:07:29 +0200

    Added v7 packages

---
+++ docker/elasticsearch/verify.py
@@ -0,0 +1,10 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+from elasticsearch7 import Elasticsearch
+from elasticsearch7_dsl import Search
+
+from opensearchpy import OpenSearch
+
+
+print("All good")
