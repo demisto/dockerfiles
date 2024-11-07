9953601a94324588ec7c2007fc9159fde17d1aa0
kobymeir <kmeir@paloaltonetworks.com>
2024-11-07 12:58:53 +0200

    take 10

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -430,6 +430,7 @@ total=$(find $SCRIPT_DIR -maxdepth 1 -mindepth 1 -type  d -print | wc -l)
 count=0
 errors=()
 for docker_dir in `find $SCRIPT_DIR -maxdepth 1 -mindepth 1 -type  d -print | sort`; do
+    echo "Checking dir: ${docker_dir} against ${DIFF_COMPARE}"
     if [[ ${DIFF_COMPARE} = "ALL" ]] || [[ $(git diff --name-status $DIFF_COMPARE -- ${docker_dir}) ]]; then
         if [ -n "${DOCKER_INCLUDE_GREP}" ] && [ -z "$(echo ${docker_dir} | grep -E ${DOCKER_INCLUDE_GREP})" ]; then
             [[ -z "$1" ]] && echo "Skipping dir: '${docker_dir}' as not included in grep expression DOCKER_INCLUDE_GREP: '${DOCKER_INCLUDE_GREP}'"
