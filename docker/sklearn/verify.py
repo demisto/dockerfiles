217da8ade04eb05458c64ee1bb2268dfd9e22f82
jlevypaloalto <jlevy@paloaltonetworks.com>
2024-11-10 14:34:40 +0200

    fix assert

--- docker/sklearn/verify.py
+++ docker/sklearn/verify.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 import os
 import multiprocessing
-assert os.environ['OPENBLAS_NUM_THREADS'] == 1, f"{os.environ['OPENBLAS_NUM_THREADS']=}"
-assert os.environ['OMP_NUM_THREADS'] == multiprocessing.cpu_count(), f"{os.environ['OMP_NUM_THREADS']=}, {multiprocessing.cpu_count()=}"
+assert int(os.environ['OPENBLAS_NUM_THREADS']) == 1, f"{os.environ['OPENBLAS_NUM_THREADS']=}"
+assert int(os.environ['OMP_NUM_THREADS']) == multiprocessing.cpu_count(), f"{os.environ['OMP_NUM_THREADS']=}, {multiprocessing.cpu_count()=}"
 
 print('All packages were imported successfully')
