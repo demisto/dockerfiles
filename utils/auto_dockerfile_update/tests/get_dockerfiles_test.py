deaa660547a55b58e5b1eb6ec3938913ad51aad6
ypreisler <ypreisler@paloaltonetworks.com>
2024-12-22 11:26:06 +0200

    fix test

--- utils/auto_dockerfile_update/tests/get_dockerfiles_test.py
+++ utils/auto_dockerfile_update/tests/get_dockerfiles_test.py
@@ -1,7 +1,7 @@
 from utils.auto_dockerfile_update.get_dockerfiles import get_docker_files
 
 
-def test_get_docker_files_multiple_from():
+def test_get_docker_files_multiple_from_internal():
     """
     Given
         - Docker file with multiple FROM statements
@@ -10,5 +10,18 @@ def test_get_docker_files_multiple_from():
     Then
         - Verify length of output list is as expected
     """
-    files_list = get_docker_files(base_path="utils/auto_dockerfile_update/tests/test_data/")
-    assert len(files_list) == 3
+    files_list = get_docker_files(base_path="utils/auto_dockerfile_update/tests/test_data/", internal=True)
+    assert len(files_list) == 2
+
+
+def test_get_docker_files_multiple_from_external():
+    """
+    Given
+        - Docker file with multiple FROM statements
+    When
+        - running get_docker_files
+    Then
+        - Verify length of output list is as expected
+    """
+    files_list = get_docker_files(base_path="utils/auto_dockerfile_update/tests/test_data/", external=True)
+    assert len(files_list) == 1
