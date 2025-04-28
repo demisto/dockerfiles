c055120d0dcddb2163b2f272bb92c025de33a261
iapt@paloaltonetworks.com <iapt@paloaltonetworks.com>
2025-04-28 11:13:50 +0300

    fix

--- docker/azure-cli/verify.py
+++ docker/azure-cli/verify.py
@@ -3,7 +3,4 @@ import msal
 from cryptography.hazmat.primitives.serialization import pkcs12
 from cryptography.hazmat.primitives import serialization
 from bs4 import BeautifulSoup
-# Make sure cryptograph works
-key = Fernet.generate_key()
-print("All is good. cryptography generated a key: {}".format(key))
-print(msal.oauth2cli.assertion.JwtAssertionCreator('', 'All is good. msal was imported successfully').algorithm)
+print('All good')
