825fd8c27d47959d4021c5c384c577034236bb17
iapt@paloaltonetworks.com <iapt@paloaltonetworks.com>
2025-04-28 14:46:23 +0300

    test

--- docker/azure-cli/verify.py
+++ docker/azure-cli/verify.py
@@ -1,6 +1,1 @@
-from cryptography.fernet import Fernet
-import msal
-from cryptography.hazmat.primitives.serialization import pkcs12
-from cryptography.hazmat.primitives import serialization
-from bs4 import BeautifulSoup
 print('All good')
