1205ab8ae125294f0b1da16cdefc5a967bd0d886
shmuel44 <skroizer@paloaltonetworks.com>
2025-09-11 13:14:57 +0300

    kusto

--- docker/pysigma/verify.py
+++ docker/pysigma/verify.py
@@ -3,7 +3,7 @@ from sigma.collection import SigmaCollection
 from sigma.backends.cortexxdr import CortexXDRBackend
 from sigma.backends.splunk import SplunkBackend
 from sigma.backends.sentinelone import SentinelOneBackend
-from sigma.backends.microsoft365defender import Microsoft365DefenderBackend
+from sigma.backends.kusto import KustoBackend
 from sigma.backends.carbonblack import CarbonBlackBackend
 from sigma.backends.QRadarAQL import QRadarAQLBackend
 from sigma.backends.elasticsearch import LuceneBackend
