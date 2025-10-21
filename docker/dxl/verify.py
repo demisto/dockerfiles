206f0f4a7c78a5f4fe38bbd616981ee6120ac30a
shmuel44 <skroizer@paloaltonetworks.com>
2025-10-21 16:05:30 +0300

    update

--- docker/dxl/verify.py
+++ docker/dxl/verify.py
@@ -2,8 +2,9 @@ from dxlclient.broker import Broker
 from dxlclient import DxlClient
 from dxlclient.client import DxlClient
 from dxlclient.client_config import DxlClientConfig
-from dxlmarclient import ConditionConstants, MarClient, OperatorConstants, ProjectionConstants, TieClient
+from dxlmarclient import ConditionConstants, MarClient, OperatorConstants, ProjectionConstants
 from dxlclient.message import Event
+from dxltieclient import TieClient
 from dxltieclient.constants import (
     AtdAttrib,
     AtdTrustLevel,
