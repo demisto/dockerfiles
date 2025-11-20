12b2936066d1e7cbd33f6b133c76c30ad18a1289
shmuel44 <skroizer@paloaltonetworks.com>
2025-11-20 13:30:17 +0200

    revert verify

--- docker/py-ews/verify.py
+++ docker/py-ews/verify.py
@@ -1,80 +1,45 @@
 import email
 import hashlib
-from email.policy import SMTP, SMTPUTF8
-from io import StringIO
+import subprocess
+import warnings
+from collections import deque
 from multiprocessing import Process
 
+import lxml
 import dateparser
-from exchangelib import (
-    BASIC,
-    DELEGATE,
-    DIGEST,
-    IMPERSONATION,
-    NTLM,
-    Body,
-    EWSDateTime,
-    EWSTimeZone,
-    FileAttachment,
-    FolderCollection,
-    HTMLBody,
-    ItemAttachment,
-    Version,
-)
-from exchangelib.errors import (
-    ErrorCannotOpenFileAttachment,
-    ErrorFolderNotFound,
-    ErrorInvalidPropertyRequest,
-    ErrorIrresolvableConflict,
-    ErrorMailboxMoveInProgress,
-    ErrorMailboxStoreUnavailable,
-    ErrorMimeContentConversionFailed,
-    ErrorNameResolutionNoResults,
-    RateLimitError,
-    TransportError,
-)
+import exchangelib
+from io import StringIO
+from exchangelib import (BASIC, DELEGATE, DIGEST, IMPERSONATION, NTLM, Account,
+                         Body, Build, Configuration, Credentials, EWSDateTime,
+                         EWSTimeZone, FileAttachment, Folder, HTMLBody,
+                         ItemAttachment, Version)
+from exchangelib.errors import (AutoDiscoverFailed, ErrorFolderNotFound,
+                                ErrorInvalidIdMalformed,
+                                ErrorInvalidPropertyRequest,
+                                ErrorIrresolvableConflict, ErrorItemNotFound,
+                                ErrorMailboxMoveInProgress,
+                                ErrorMailboxStoreUnavailable,
+                                ErrorNameResolutionNoResults, RateLimitError,
+                                ResponseMessageError, TransportError)
 from exchangelib.items import Contact, Item, Message
+from exchangelib.protocol import BaseProtocol, Protocol
 from exchangelib.services import EWSService
+from exchangelib.services.common import EWSAccountService
 from exchangelib.util import add_xml_child, create_element
-from exchangelib.version import (
-    EXCHANGE_2007,
-    EXCHANGE_2010,
-    EXCHANGE_2010_SP2,
-    EXCHANGE_2013,
-    EXCHANGE_2013_SP1,
-    EXCHANGE_2016,
-    EXCHANGE_2019,
-)
-from exchangelib.version import VERSIONS as EXC_VERSIONS
+from exchangelib.version import (EXCHANGE_2007, EXCHANGE_2010,
+                                 EXCHANGE_2010_SP2, EXCHANGE_2013,
+                                 EXCHANGE_2016, EXCHANGE_2019)
+from future import utils as future_utils
 from requests.exceptions import ConnectionError
-
+from _sqlite3 import *
 # verify that we support dh 1024
 import requests
-import ssl
-from urllib3.util import ssl_ as urllib3_ssl
-
-# Python 3.12 ships with stricter OpenSSL defaults (typically SECLEVEL=2), which
-# reject 1024-bit DH. The dh1024.badssl.com check then fails with
-# "ssl.SSLError: DH_KEY_TOO_SMALL". For this verification only, we relax to
-# SECLEVEL=1 by setting urllib3's DEFAULT_CIPHERS and using a custom SSLContext.
-urllib3_ssl.DEFAULT_CIPHERS = (
-    'DEFAULT:@SECLEVEL=1:'
-    'ECDHE+AESGCM:ECDHE+CHACHA20:'
-    'DHE+AESGCM:DHE+CHACHA20:'
-    'ECDH+AESGCM:DH+AESGCM:'
-    'ECDH+AES:DH+AES:'
-    'RSA+AESGCM:RSA+AES:'
-    '!aNULL:!eNULL:!MD5:!DSS'
-)
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS ='@SECLEVEL=1:ECDHE+AESGCM:ECDHE+CHACHA20:DHE+AESGCM:DHE+CHACHA20:ECDH+AESGCM:DH+AESGCM:' \
+                         'ECDH+AES:DH+AES:RSA+ANESGCM:RSA+AES:!aNULL:!eNULL:!MD5:!DSS'
+                         # same string used in CSP, override py3 hardening
 requests.packages.urllib3.disable_warnings()
-
-ctx = ssl.create_default_context()
-ctx.set_ciphers('DEFAULT:@SECLEVEL=1')
-session = requests.Session()
-adapter = requests.adapters.HTTPAdapter()
-adapter.init_poolmanager(10, 10, ssl_context=ctx)
-session.mount('https://', adapter)
-res = session.get('https://dh1024.badssl.com/', verify=False)
+res = requests.get('https://dh1024.badssl.com/', verify=False)
 res.raise_for_status()
 
-# verify dateparser works. We had a case that it failed with timezone issues
+# verify dateaparser works. We had a case that it failed with timezone issues
 dateparser.parse("10 minutes")
