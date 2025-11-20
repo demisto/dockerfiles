37c392f97eb0a9a12d5bb342caa1c288b3b3b8b4
shmuel44 <skroizer@paloaltonetworks.com>
2025-11-20 13:59:17 +0200

    new docker

--- docker/py-ews/verify.py
+++ docker/py-ews/verify.py
@@ -34,7 +34,7 @@ from requests.exceptions import ConnectionError
 from _sqlite3 import *
 # verify that we support dh 1024
 import requests
-requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS ='@SECLEVEL=1:ECDHE+AESGCM:ECDHE+CHACHA20:DHE+AESGCM:DHE+CHACHA20:ECDH+AESGCM:DH+AESGCM:' \
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS ='@SECLEVEL=0:ECDHE+AESGCM:ECDHE+CHACHA20:DHE+AESGCM:DHE+CHACHA20:ECDH+AESGCM:DH+AESGCM:' \
                          'ECDH+AES:DH+AES:RSA+ANESGCM:RSA+AES:!aNULL:!eNULL:!MD5:!DSS'
                          # same string used in CSP, override py3 hardening
 requests.packages.urllib3.disable_warnings()
