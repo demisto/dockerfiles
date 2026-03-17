2caa4862920a777300492be145a2955dc95050e0
YairGlik <yair.glik33@gmail.com>
2026-03-17 13:16:41 +0200

    fix

--- docker/aquatone/verify.py
+++ docker/aquatone/verify.py
@@ -2,5 +2,5 @@ from subprocess import Popen, PIPE
 cmd = ['aquatone-discover', '--domain', 'example.com']
 p = Popen(cmd, stdout=PIPE, stderr=PIPE, encoding="utf-8")
 stdout, stderr = p.communicate()
-print("All is good. aquatone-discover initialized")
+print("All is good. aquatone-discover initialized ")
 
