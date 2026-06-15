5cc88dcf13cdcca3b23dda4cead4b6f2fa064802
Adi <aedri@paloaltonetworks.com>
2026-06-15 10:38:17 +0300

    Update

--- docker/netutils/verify.py
+++ docker/netutils/verify.py
@@ -21,3 +21,18 @@ import aiohttp
 
 # verify aiolimiter
 from aiolimiter import AsyncLimiter
+
+import dns.rdataclass  # noqa: F401
+import dns.rdata  # noqa: F401
+
+assert dns.rdatatype.from_text("A") == dns.rdatatype.RdataType.A
+assert dns.rdataclass.from_text("IN") == dns.rdataclass.RdataClass.IN
+
+# --- websockets (CertStream, ProofpointEmailSecurityEventCollector, RetarusSecureEmailGateway) ---
+from websockets.sync.client import connect  # noqa: F401
+from websockets.sync.connection import Connection  # noqa: F401
+from websockets.exceptions import InvalidStatus, ConnectionClosed  # noqa: F401
+from websockets import Data  # noqa: F401
+
+
+print("netutils is good")
