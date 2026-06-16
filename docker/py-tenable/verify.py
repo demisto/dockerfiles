25c4a259881ee212ff48f410e15dae8f19014f45
gbouzar <113393855+gbouzar@users.noreply.github.com>
2026-06-16 07:35:41 -0700

    Update verify.py

--- docker/py-tenable/verify.py
+++ docker/py-tenable/verify.py
@@ -1,6 +1,7 @@
 import sys
 # Import the packages you added to your Pipfile to make sure they load
 import requests 
+from tenable.tenableone import TenableOne
 
 print("Sanity check passed: Dependencies loaded successfully inside the container!")
 sys.exit(0) # 0 tells the automated pipeline that everything works perfectly
