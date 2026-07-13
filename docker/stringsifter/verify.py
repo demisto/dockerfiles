a1f449169d6c98b7980d715aa145b88a349d84bc
shmuel44 <skroizer@paloaltonetworks.com>
2026-07-13 22:26:57 +0300

    fix

--- docker/stringsifter/verify.py
+++ docker/stringsifter/verify.py
@@ -1,12 +1,13 @@
 from subprocess import Popen, PIPE
 
-command = "flarestrings requirements.txt"
+command = "flarestrings requirements.txt | rank_strings --scores"
 
 process = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
 
-return_code = process.returncode
-if return_code != 0:
+if process.returncode != 0:
     raise Exception("verification failed")
+if stderr:
+    raise Exception(f"rank_strings produced stderr output: {stderr.decode(errors='replace')}")
 
 print("Successfully executed stringsifter command")
