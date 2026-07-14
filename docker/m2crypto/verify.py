5a7f224bddfaeb7e3c9a0f7f0971a6f75de9acd2
shmuel44 <skroizer@paloaltonetworks.com>
2026-07-14 11:57:58 +0300

    update

--- docker/m2crypto/verify.py
+++ docker/m2crypto/verify.py
@@ -1,4 +1,16 @@
-import M2Crypto
+import mimetypes
+import quopri
+import uuid
+from email import encoders, message_from_string
+from email.message import Message
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from tempfile import NamedTemporaryFile
+
+import pytz
+from charset_normalizer import from_bytes
+from M2Crypto import BIO, SMIME, X509
 
 
 print("All is good. M2Crypto imported successfully")
