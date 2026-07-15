037bae90ae7e69cddc0885451fc713a699755bf9
Adi <aedri@paloaltonetworks.com>
2026-07-15 11:35:16 +0300

    update verify

--- docker/flask-nginx/verify.py
+++ docker/flask-nginx/verify.py
@@ -10,3 +10,8 @@ assert 'nginx/1.' in res
 # verify that nginx test passes
 res = subprocess.check_output(['nginx', '-t'], text=True, stderr=subprocess.STDOUT)
 print(res)
+
+# verify that musl is pinned to 1.2.5 (XSUP-72663)
+res = subprocess.check_output(['apk', 'version', 'musl'], text=True, stderr=subprocess.STDOUT)
+print(res)
+assert '1.2.5' in res
