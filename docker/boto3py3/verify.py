b87c9a28ac83b03265afd80fb3927b9b0e8e1ac6
Yaakov Praisler <59408745+yaakovpraisler@users.noreply.github.com>
2026-09-07 10:58:44 +0300

    Revert add awscli to boto3py3 (#46297)
    * Revert "Add awscli package to boto3py3 (#45688)"
    
    This reverts commit 15b02a9864d62a93fb0dd037f159fcf17685b7c3.
    
    * lock

--- docker/boto3py3/verify.py
+++ docker/boto3py3/verify.py
@@ -1,10 +1,2 @@
-import subprocess
-
 import boto3
-
-print('boto3 import successful')
-
-result = subprocess.run(['aws', '--version'], capture_output=True, text=True)
-if result.returncode != 0:
-    raise RuntimeError(f'awscli was not installed properly: {result.stderr}')
-print(f'awscli is good: {result.stdout or result.stderr}')
+print('boto3py3 is good!!!')
